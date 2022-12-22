899e491c23580696d1fd3ffa2ad8b44516527d27
hugotomita1201 <tomitahugo@gmail.com>
2022-12-21 16:49:32 -0800

    put entire eda_py file in main app.py

--- app.py
+++ app.py
@@ -17,7 +17,63 @@ import plotly.express as px
 
 # importing certain variables from eda_py which is the .py version of the EDA jupyter notebook within
 # the project-1 folder
-from eda_py import sector_histogram, data, scatter_100, scatter_50, scatter
+#from eda_py import sector_histogram, data, scatter_100, scatter_50, scatter
+
+# from here to line 76 is eda_py.py file
+data = pd.read_csv(r'/Users/hugo/python_projects/spy_distribution/spy.csv')
+data = pd.DataFrame(data)
+data
+
+data.info()
+
+data.columns = data.columns.str.lower()
+data['sector'] = data['sector'].str.lower()
+data['price/book'] = data['price/book'].fillna(0)
+
+sectors = data.groupby('sector').agg(
+    {'symbol': 'count'}).sort_index(ascending=True)
+
+sector_fig = px.bar(sectors, labels={'value': '# of companies',
+                                     'variable': 'legend',
+                                     'symbol': '# of symbols', },
+                    title='Distribution of Companies in the S&P500 by Sector')
+
+sector_fig.show()
+
+x = data.groupby('sector')['market cap'].count()
+y = data.groupby('sector')['market cap'].sum()
+
+
+mean_market_cap = pd.merge(x, y, left_index=True, right_index=True)
+
+mean_market_cap['average_cap'] = mean_market_cap['market cap_y']//mean_market_cap['market cap_x']
+
+avg_cap_fig = px.bar(mean_market_cap, y='average_cap',
+                     labels={'average_cap': 'Market Cap',
+                             'sector': 'Sector'},
+                     title='Average Stock Market Cap of Each Sector')
+avg_cap_fig.show()
+
+sector_histogram = px.histogram(data, x='sector',
+                                labels={'count': 'Number of Companies',
+                                        'sector': 'Sector'},
+                                title='Number of Companies per Sector in the S&p 500')
+sector_histogram.show()
+
+scatter = px.scatter(data, x='price', y='market cap', opacity=0.7,
+                     labels={'market cap': 'Market Cap', 'price': 'Price'}, title='Scatterplot of Market Caps in the S&P500')
+scatter
+
+data_below_50 = data.query("`market cap` <= 50000000000")
+
+scatter_50 = px.scatter(data_below_50, x='price', y='market cap', opacity=0.7,
+                        labels={'market cap': 'Market Cap', 'price': 'Price'})
+scatter_50
+data_below_100 = data.query("`market cap` <= 100000000000")
+scatter_100 = px.scatter(data_below_100, x='price', y='market cap', opacity=0.7,
+                         labels={'market cap': 'Market Cap', 'price': 'Price'})
+scatter_100
+
 
 # title of the website
 st.header("S&P 500 COMPANIES WITH FINANCIAL INFORMATION")
