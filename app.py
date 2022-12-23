a3f23699e68d31acda93bc799ce15bd3cf860715
hugotomita1201 <tomitahugo@gmail.com>
2022-12-22 16:15:39 -0800

    changes to app and ipynb changes#4

--- app.py
+++ app.py
@@ -11,7 +11,7 @@
 # a histogram and multiple scatter plots.
 
 # I have imported modules containing the charts shown in this app through
-# the eda_py.py file.
+# the eda_py.py file
 
 
 import streamlit as st
@@ -49,6 +49,50 @@ below_100 = st.checkbox("Show only market caps below 100 billion?")
 # using imported scatter plot to show
 if below_100:
     st.plotly_chart(scatter_100)
+
+
+# now working on filter section where I want to compare 2 symbols and their market caps in a bar chart.
+# title
+st.title("Compare Market Caps of Individual Stocks")
+
+# coming up with a dataframe of all unique symbols and putting them into list format
+# point here is to just get the symbol/sector names to be able to put into st.checkbox to compare
+symbols = data['symbol'].unique().tolist()
+sectors = data['sector'].unique().tolist()
+
+# first giving the user a selectbox to select from sector to narrow down search from 500 names (total in spy) to just the ones
+# within the selected in the specific sector
+sector_1 = st.selectbox("select sector for 1st company: ", sectors)
+
+# using a filter to create boolean values for all symbols where the column 'sector' is the one selected in the selectbox above
+# and then creating a dataframe from the filtered data
+sector_1_filtered = (data['sector'] == sector_1)
+data_sector_1 = data[sector_1_filtered]
+
+# and then letting user choose symbol_1 for comparison
+symbol_1 = st.selectbox(
+    "select 1st symbol to compare: ", data_sector_1['symbol'], key=1)
+
+# same thing but for 2nd symbol
+sector_2 = st.selectbox("select sector for 2nd company: ", sectors)
+sector_2_filtered = (data['sector'] == sector_2)
+data_sector_2 = data[sector_2_filtered]
+
+
+symbol_2 = st.selectbox(
+    "select 2nd symbol to compare: ", data_sector_2['symbol'], key=2)
+
+# using a mask filter to get a list of boolean values for all index values in the main dataframe
+# point is to be able to access the market cap values for all indexes where the boolean value is True (the symbols that were selected in
+# st.checkbox)
+mask_filter = (data['symbol'] == symbol_1) | (data['symbol'] == symbol_2)
+data_filtered = data[mask_filter]
+
+# and then using st.write and px.bar chart to compare the symbol and market cap columns within the data_filtered
+st.write(px.bar(data_filtered,
+                x='symbol',
+                y='market cap'))
+
 
 # conclusions:
 # Through the histogram which presents the sector distributions of the SPY 500, we
